cb81793c357a034be73186ee4339949ff31f43e5
Guillaume Witz <guiwitz@users.noreply.github.com>
2019-10-10 15:03:22 +0200

    import data automatically for demo notebook

---
+++ scripts/demo_import.py
@@ -0,0 +1,19 @@
+import requests
+import os
+
+if not os.path.isdir('Sporefolder/SporesA'):
+    os.makedirs('Sporefolder/SporesA')
+if not os.path.isdir('Sporefolder/SporesB'):
+    os.makedirs('Sporefolder/SporesB')
+    
+myfile = requests.get('https://www.dropbox.com/s/e1y882oawwxmtn6/spore1.jpg?dl=1', allow_redirects=True)
+open('Sporefolder/SporesA/spore1.jpg', 'wb').write(myfile.content)
+
+myfile = requests.get('https://www.dropbox.com/s/f7yufnypmbxfu06/spore2.jpg?dl=1', allow_redirects=True)
+open('Sporefolder/SporesA/spore2.jpg', 'wb').write(myfile.content)
+
+myfile = requests.get('https://www.dropbox.com/s/e1y882oawwxmtn6/spore1.jpg?dl=1', allow_redirects=True)
+open('Sporefolder/SporesB/spore1.jpg', 'wb').write(myfile.content)
+
+myfile = requests.get('https://www.dropbox.com/s/f7yufnypmbxfu06/spore2.jpg?dl=1', allow_redirects=True)
+open('Sporefolder/SporesB/spore2.jpg', 'wb').write(myfile.content)
