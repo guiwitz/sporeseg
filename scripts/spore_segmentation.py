77d91186df60f7ebaf75c1e203fb22ecb8acc086
Guillaume Witz <guiwitz@users.noreply.github.com>
2019-10-30 17:47:56 +0100

    convexity option

--- scripts/spore_segmentation.py
+++ scripts/spore_segmentation.py
@@ -4,6 +4,7 @@ from spores.sporeClass import Spore
 parser=argparse.ArgumentParser()
 parser.add_argument('exp_folder', type = str, help='Experiment folder')
 parser.add_argument('result_folder', type = str, help='Result storage folder')
+parser.add_argument('--convexity', type = float, help='Convexity threshold')
 parser.add_argument('--show_output', action='store_true', help='show plots')
 args=parser.parse_args()
 
@@ -12,6 +13,7 @@ spores = Spore()
 exp_folder = args.exp_folder
 result_folder = args.result_folder
 
+spores.convexity = args.convexity
 spores.show_output = args.show_output
 
 spores.analyse_spore_folder(exp_folder, result_folder)
