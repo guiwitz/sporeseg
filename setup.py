63418d236849cf71c05c12f4533ac74dac49865e
guiwitz <guiwitz@users.noreply.github.com>
2021-05-03 09:05:43 +0200

    update setup, rename package to sporeseg

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-from setuptools import setup
+import setuptools
 
-setup(name='spores',
-      version='0.1',
-      description='Automated analysis of microscopy images of spores',
-      url='https://github.com/guiwitz/sporeseg',
-      author='Guillaume Witz',
-      author_email='',
-      license='BSD3',
-      packages=['spores'],
-      zip_safe=False,
-      install_requires=['numpy','scikit-image','scikit-learn','jupyter','jupyterlab','pandas','tifffile','tqdm', 'matplotlib','requests'],
-      )
+setuptools.setup()
